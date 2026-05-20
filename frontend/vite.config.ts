d77b5fcd8b2e525498b498945c2ba468dce694d1
Vamshi Renduchintala <93363393+Vrendu@users.noreply.github.com>
2026-05-19 19:47:44 -0700

    adjust frontend port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    preview: {
+      host: '0.0.0.0',
+      port: Number(process.env.PORT) || 4173,
+    },
     server: {
       proxy: {
         '/api': {
